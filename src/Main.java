7ecff6ea511194f464bc34aa4934fc58c96be2d4
Aignacio01 <AignacioG01@gmail.com>
2026-04-13 18:51:50 -0400

    feat: se actualiza main con opcion de registro camion y moto

--- src/Main.java
+++ src/Main.java
@@ -28,13 +28,13 @@ public class Main {
 
             switch (opcion) {
                 case 1 -> {// Allan
-
+                    registrarCamion(sc, vehiculos);
                 }
                 case 2 -> { // Felipe
 
                 }
                 case 3 -> {// Bruno
-
+                    registrarMoto(sc, vehiculos);
                 }
                 case 4 -> {// Dafne
                     System.out.println("\n==== LISTA DE VEHICULOS =====");
@@ -71,4 +71,59 @@ public class Main {
            vehiculo.mostrarDetalle();
        }
     }
+
+    // Funciones
+    public static void registrarCamion(Scanner sc, List<Vehiculo> vehiculos) {
+  
+    // pedir patente 
+    System.out.println("Ingrese la patente:");
+    String patente = sc.nextLine().trim();
+    // Pedir marca
+    System.out.print("Ingrese marca: ");
+    String marca = sc.nextLine().trim();
+    // Pedir modelo
+    System.out.print("Ingrese modelo: ");
+    String modelo = sc.nextLine().trim();
+    // pedir capacidad
+    System.out.println("Ingrese la capacidad de carga (kg): ");
+    Double capacidad = sc.nextDouble();
+    if (capacidad > 0 ) {
+        System.out.println("");
+    } else {
+        System.out.println("La cantidad de carga que llevas no puede ser negativa.");
+    }
+    // pedir ejes
+    int ejes = 0;
+    System.out.println("Ingrese la cantidad de ejes de su vehiculo: ");
+    ejes = sc.nextInt();
+    if (ejes > 1 ) {
+        System.out.println("");
+    } else { 
+        System.out.println("Cantidad de ejes erronea, debes ingresar ejes mayor a 1.");
+    }
+
+
+    System.out.println( "informacion del camion registrado: " + patente + " | " + marca + " | " + modelo + " | " + capacidad + "kg | " + ejes + " ejes" );
+
+}
+
+    public static void registrarMoto(Scanner sc, List<Vehiculo> vehiculos) {
+         // pedir patente 
+    System.out.println("Ingrese la patente:");
+    String patente = sc.nextLine().trim();
+    // Pedir marca
+    System.out.print("Ingrese marca: ");
+    String marca = sc.nextLine().trim();
+    // Pedir modelo
+    System.out.print("Ingrese modelo: ");
+    String modelo = sc.nextLine().trim();
+    // pedir caja termica
+    System.out.println("Tiene caja termina S/N: ");
+    String ct = sc.nextLine().trim().toUpperCase();
+    boolean tieneCaja = ct.equals("S");
+
+    System.out.println( "informacion de la moto registrada: " + patente + " | " + marca + " | " + modelo + " | " + tieneCaja );
+
+}
+
 }
